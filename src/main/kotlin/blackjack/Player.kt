9880f266ef683fee8eeac4c4c6fc09a51f39628a
sjjeong <sjjeong1225@gmail.com>
2023-07-04 17:46:11 +0900

    step2: A 계산할 때 사용하는 상수를 const val로 추출

--- src/main/kotlin/blackjack/Player.kt
+++ src/main/kotlin/blackjack/Player.kt
@@ -8,8 +8,9 @@ data class Player(
     val score: Int
         get() {
             var score = cardList.sumOf { it.rank.point }
-            if (cardList.any { it.rank == Rank.ACE } && score <= 11) {
-                score += 10
+
+            if (cardList.any { it.rank == Rank.ACE } && score <= BLACKJACK_SCORE - ACE_EXTRA_POINT) {
+                score += ACE_EXTRA_POINT
             }
             return score
         }
@@ -24,6 +25,8 @@ data class Player(
     }
 
     companion object {
+        private const val BLACKJACK_SCORE = 21
+        private const val ACE_EXTRA_POINT = 10
         fun of(playerString: String): List<Player> {
             return playerString.split(",")
                 .map { Player(it.trim()) }
