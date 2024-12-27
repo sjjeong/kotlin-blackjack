8b5a03ba64807d10ad5b3671b9af3ca3f26180ba
SeokJun Jeong <sjjeong1225@gmail.com>
2024-12-27 17:17:40 +0900

    refactor: 예외 발생시 메시지 추가

--- src/main/kotlin/blackjack/domain/BlackjackShoe.kt
+++ src/main/kotlin/blackjack/domain/BlackjackShoe.kt
@@ -4,7 +4,9 @@ class BlackjackShoe(deck: Int = DEFAULT_DECK_OF_CARDS) {
     private val cards: MutableList<Card> = mutableListOf()
 
     init {
-        require(deck in 1..MAX_DECK_OF_CARDS)
+        require(deck in MIN_DECK_OF_CARDS..MAX_DECK_OF_CARDS) {
+            "블랙잭 슈에는 최소${MIN_DECK_OF_CARDS}개, 최대${MAX_DECK_OF_CARDS}개의 덱이 들어갈 수 있습니다."
+        }
         for (suit in Suit.entries) {
             for (rank in Rank.entries) {
                 cards.addAll(List(deck) { Card(suit, rank) })
@@ -20,6 +22,7 @@ class BlackjackShoe(deck: Int = DEFAULT_DECK_OF_CARDS) {
 
     companion object {
         private const val DEFAULT_DECK_OF_CARDS = 6
+        private const val MIN_DECK_OF_CARDS = 1
         private const val MAX_DECK_OF_CARDS = 8
     }
 }
